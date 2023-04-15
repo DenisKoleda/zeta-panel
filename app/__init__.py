d4dad40367fcc34dc17ecfe32c592c53fff90d1b
ReDGamerR <super.koleda92@yandex.ru>
2023-04-15 11:17:39 +0300

    Мелкие исправления + добавления новых страниц

---
+++ app/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+mail = Mail()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Конфигурация приложения:
+    app.config.update(
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=587,
+        MAIL_USERNAME='thekolesnikovkirill@gmail.com',
+        MAIL_PASSWORD='ynmfpimpqkhrdugs',
+        MAIL_USE_TLS=True,
+        MAIL_USE_SSL=False,
+        SECRET_KEY='0f0809b30cb03b7c90d77ecfb35a10a4',
+        SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite'
+    )
+
+    # Инициализация экземпляров:
+    db.init_app(app)
+    mail.init_app(app)
+
+    # Создание экземпляра LoginManager для работы с аутентификацией пользователей:
+    login_manager = LoginManager(app)
+    # Перенаправление на страницу входа при отсутствии доступа
+    login_manager.login_view = 'auth.login'
+
+    # Загрузка пользователя для каждого запроса:
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Регистрация синихпринтов для работы с авторизацией и главным приложением:
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+from flask_login import UserMixin
+import jwt
+from time import time
+from flask import current_app
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    username = db.Column(db.String(1000))
