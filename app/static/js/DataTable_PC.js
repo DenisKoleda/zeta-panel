49905a1eebbdf094df526b98ca14f7ba513a1079
Denis Koleda <super.koleda92@yandex.ru>
2023-04-22 13:55:36 +0300

    cleanup

--- app/static/js/DataTable_PC.js
+++ app/static/js/DataTable_PC.js
@@ -28,54 +28,35 @@ $(document).ready(function () {
         { "data": "user" },
         { "data": "smart" }
       ],
-      initComplete: function () {
-        var api = this.api();
-
-        // Поиск
-        api
-          .columns()
-          .eq(0)
-          .each(function (colIdx) {
-            // Set the header cell to contain the input element
-            var cell = $('.filters th').eq(
-              $(api.column(colIdx).header()).index()
-            );
-            var title = $(cell).text();
-            $(cell).html('<input type="text" placeholder="' + title + '" />');
-
-            // On every keypress in this input
-            $(
-              'input',
-              $('.filters th').eq($(api.column(colIdx).header()).index())
-            )
-              .off('keyup change')
-              .on('change', function (e) {
-                // Get the search value
-                $(this).attr('title', $(this).val());
-                var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-                var cursorPosition = this.selectionStart;
-                // Search the column for that value
-                api
-                  .column(colIdx)
-                  .search(
-                    this.value != ''
-                      ? regexr.replace('{search}', '(((' + this.value + ')))')
-                      : '',
-                    this.value != '',
-                    this.value == ''
-                  )
-                  .draw();
-              })
-              .on('keyup', function (e) {
-                e.stopPropagation();
-
-                $(this).trigger('change');
-                $(this)
-                  .focus()[0]
-              });
+      initComplete: function() {
+        var dataTable = this.api();
+      
+        // Add input elements to table headers
+        dataTable.columns().eq(0).each(function(colIndex) {
+          var columnHeader = $('.filters th').eq($(dataTable.column(colIndex).header()).index());
+          var title = $(columnHeader).text();
+          $(columnHeader).html('<input type="text" placeholder="' + title + '" />');
+      
+          // Handle input change event
+          $('input', $(columnHeader)).off('keyup change').on('change', function(event) {
+            // Get the search value
+            $(this).attr('title', $(this).val());
+            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+            var searchValue = this.value != '' ? '(((' + this.value + ')))' : '';
+      
+            // Search the column for the search value
+            dataTable.column(colIndex)
+              .search(searchValue, this.value != '', this.value == '')
+              .draw();
+          }).on('keyup', function(event) {
+            event.stopPropagation();
+      
+            $(this).trigger('change');
+            $(this).focus()[0];
           });
+        });
       },
+      
     });
     // Форма добавления элемента
     $('#addForm').submit(function (event) {
