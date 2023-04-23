6b0ddc3c45bb7c85e507b1f2f8a016c4616883bb
Denis Koleda <super.koleda92@yandex.ru>
2023-04-23 23:19:14 +0300

    fix mail

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     # Конфигурация приложения:
     app.config.update(
         MAIL_SERVER='smtp.yandex.ru',
-        MAIL_PORT=465,
+        MAIL_PORT=587,
         MAIL_USERNAME='info@zetalink.ru',
         MAIL_PASSWORD='datacenter13',
         MAIL_USE_TLS=True,
