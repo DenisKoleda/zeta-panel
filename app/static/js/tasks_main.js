5cfd10e619d7ca37f885406b9b229184d526db39
Denis Koleda <super.koleda92@yandex.ru>
2023-05-01 19:24:18 +0300

    fix

--- app/static/js/tasks_main.js
+++ app/static/js/tasks_main.js
@@ -1,59 +1,279 @@
 $(document).ready(function () {
   var apiEndpoint = '/api/tasks/get';
-  // Клонирование thead таблицы
-  $('#myTable thead tr')
-    .clone(true)
-    .addClass('filters')
-    .appendTo('#myTable thead');
 
   // отправляем GET запрос на сервер и получаем данные
   $.get(apiEndpoint, function (data) {
-    $('#myTable').DataTable({
-      orderCellsTop: true,
+    var table = $('#myTable').DataTable({
       dom: '<"justify-content-between align-items-center"lfB><"table-responsive"rt><"justify-content-between align-items-center"ip>',
       buttons:[
-        'createState', 'savedStates', 'copy', 'excel', 'print', 'colvis'
+        {
+          extend: 'searchPanes',
+          config: {
+            cascadePanes: true,
+            viewTotal: true,
+          }
+        },
+          'createState', 'savedStates', 'copy', 
+        {
+          extend: 'excelHtml5',
+          autoFilter: true,
+          sheetName: 'Exported data'
+        }, 'print', 'colvis'
         
       ],
-      colReorder: true,
-      //responsive: true,
+      stateSave: true,
+      responsive: true,
       paging: true,
       searching: true,
       ordering: true,
       data: data,
       language: {
-          "sProcessing":   "Обработка...",
-          "sLengthMenu":   "Показать _MENU_ записей",
-          "sZeroRecords":  "Записи отсутствуют.",
-          "sInfo":         "Записи с _START_ до _END_ из _TOTAL_ записей",
-          "sInfoEmpty":    "Записи с 0 до 0 из 0 записей",
-          "sInfoFiltered": "(отфильтровано из _MAX_ записей)",
-          "sInfoPostFix":  "",
-          "sSearch":       "Поиск:",
-          "sUrl":          "",
-          "oPaginate": {
-              "sFirst":    "Первая",
-              "sPrevious": "Предыдущая",
-              "sNext":     "Следующая",
-              "sLast":     "Последняя"
-          },
-          "buttons": {
-            "removeState": "Удалить состояние",
-            "updateState": "Обновить состояние",
-            "stateRestore": "Состояние %d",
-            "renameState": "Переименовать состояние",
-            "createState": "Сохранить состояние",
-            "savedStates": "Сохраненные состояния",
-            "create": "Создать",
-            "edit": "Редактировать",
-            "remove": "Удалить",
+        "processing": "Подождите...",
+        "search": "Поиск:",
+        "lengthMenu": "Показать _MENU_ записей",
+        "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+        "infoEmpty": "Записи с 0 до 0 из 0 записей",
+        "infoFiltered": "(отфильтровано из _MAX_ записей)",
+        "loadingRecords": "Загрузка записей...",
+        "zeroRecords": "Записи отсутствуют.",
+        "emptyTable": "В таблице отсутствуют данные",
+        "paginate": {
+            "first": "Первая",
+            "previous": "Предыдущая",
+            "next": "Следующая",
+            "last": "Последняя"
+        },
+        "aria": {
+            "sortAscending": ": активировать для сортировки столбца по возрастанию",
+            "sortDescending": ": активировать для сортировки столбца по убыванию"
+        },
+        "select": {
+            "rows": {
+                "_": "Выбрано записей: %d",
+                "1": "Выбрана одна запись"
+            },
+            "cells": {
+                "_": "Выбрано %d ячеек",
+                "1": "Выбрана 1 ячейка "
+            },
+            "columns": {
+                "1": "Выбран 1 столбец ",
+                "_": "Выбрано %d столбцов "
+            }
+        },
+        "searchBuilder": {
+            "conditions": {
+                "string": {
+                    "startsWith": "Начинается с",
+                    "contains": "Содержит",
+                    "empty": "Пусто",
+                    "endsWith": "Заканчивается на",
+                    "equals": "Равно",
+                    "not": "Не",
+                    "notEmpty": "Не пусто",
+                    "notContains": "Не содержит",
+                    "notStartsWith": "Не начинается на",
+                    "notEndsWith": "Не заканчивается на"
+                },
+                "date": {
+                    "after": "После",
+                    "before": "До",
+                    "between": "Между",
+                    "empty": "Пусто",
+                    "equals": "Равно",
+                    "not": "Не",
+                    "notBetween": "Не между",
+                    "notEmpty": "Не пусто"
+                },
+                "number": {
+                    "empty": "Пусто",
+                    "equals": "Равно",
+                    "gt": "Больше чем",
+                    "gte": "Больше, чем равно",
+                    "lt": "Меньше чем",
+                    "lte": "Меньше, чем равно",
+                    "not": "Не",
+                    "notEmpty": "Не пусто",
+                    "between": "Между",
+                    "notBetween": "Не между ними"
+                },
+                "array": {
+                    "equals": "Равно",
+                    "empty": "Пусто",
+                    "contains": "Содержит",
+                    "not": "Не равно",
+                    "notEmpty": "Не пусто",
+                    "without": "Без"
+                }
+            },
+            "data": "Данные",
+            "deleteTitle": "Удалить условие фильтрации",
+            "logicAnd": "И",
+            "logicOr": "Или",
+            "title": {
+                "0": "Конструктор поиска",
+                "_": "Конструктор поиска (%d)"
+            },
+            "value": "Значение",
+            "add": "Добавить условие",
+            "button": {
+                "0": "Конструктор поиска",
+                "_": "Конструктор поиска (%d)"
+            },
+            "clearAll": "Очистить всё",
+            "condition": "Условие",
+            "leftTitle": "Превосходные критерии",
+            "rightTitle": "Критерии отступа"
+        },
+        "searchPanes": {
+            "clearMessage": "Очистить всё",
+            "collapse": {
+                "0": "Панели поиска",
+                "_": "Панели поиска (%d)"
+            },
+            "count": "{total}",
+            "countFiltered": "{shown} ({total})",
+            "emptyPanes": "Нет панелей поиска",
+            "loadMessage": "Загрузка панелей поиска",
+            "title": "Фильтры активны - %d",
+            "showMessage": "Показать все",
+            "collapseMessage": "Скрыть все"
+        },
+        "buttons": {
+            "pdf": "PDF",
+            "print": "Печать",
+            "collection": "Коллекция <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
+            "colvis": "Видимость столбцов",
+            "colvisRestore": "Восстановить видимость",
             "copy": "Копировать",
+            "copyKeys": "Нажмите ctrl or u2318 + C, чтобы скопировать данные таблицы в буфер обмена.  Для отмены, щелкните по сообщению или нажмите escape.",
+            "copyTitle": "Скопировать в буфер обмена",
             "csv": "CSV",
             "excel": "Excel",
-            "print": "Печать",
-            "colvis": "Видимость колонок"
-          }
-      },
+            "pageLength": {
+                "-1": "Показать все строки",
+                "_": "Показать %d строк",
+                "1": "Показать 1 строку"
+            },
+            "removeState": "Удалить",
+            "renameState": "Переименовать",
+            "copySuccess": {
+                "1": "Строка скопирована в буфер обмена",
+                "_": "Скопировано %d строк в буфер обмена"
+            },
+            "createState": "Сохранить состояние",
+            "removeAllStates": "Удалить все состояния",
+            "savedStates": "Сохраненные состояния",
+            "stateRestore": "Состояние %d",
+            "updateState": "Обновить"
+        },
+        "decimal": ".",
+        "infoThousands": ",",
+        "autoFill": {
+            "cancel": "Отменить",
+            "fill": "Заполнить все ячейки <i>%d<i><\/i><\/i>",
+            "fillHorizontal": "Заполнить ячейки по горизонтали",
+            "fillVertical": "Заполнить ячейки по вертикали",
+            "info": "Информация"
+        },
+        "datetime": {
+            "previous": "Предыдущий",
+            "next": "Следующий",
+            "hours": "Часы",
+            "minutes": "Минуты",
+            "seconds": "Секунды",
+            "unknown": "Неизвестный",
+            "amPm": [
+                "AM",
+                "PM"
+            ],
+            "months": {
+                "0": "Январь",
+                "1": "Февраль",
+                "10": "Ноябрь",
+                "11": "Декабрь",
+                "2": "Март",
+                "3": "Апрель",
+                "4": "Май",
+                "5": "Июнь",
+                "6": "Июль",
+                "7": "Август",
+                "8": "Сентябрь",
+                "9": "Октябрь"
+            },
+            "weekdays": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ]
+        },
+        "editor": {
+            "close": "Закрыть",
+            "create": {
+                "button": "Новый",
+                "title": "Создать новую запись",
+                "submit": "Создать"
+            },
+            "edit": {
+                "button": "Изменить",
+                "title": "Изменить запись",
+                "submit": "Изменить"
+            },
+            "remove": {
+                "button": "Удалить",
+                "title": "Удалить",
+                "submit": "Удалить",
+                "confirm": {
+                    "_": "Вы точно хотите удалить %d строк?",
+                    "1": "Вы точно хотите удалить 1 строку?"
+                }
+            },
+            "multi": {
+                "restore": "Отменить изменения",
+                "title": "Несколько значений",
+                "noMulti": "Это поле должно редактироватся отдельно, а не как часть групы",
+                "info": "Выбранные элементы содержат разные значения для этого входа.  Чтобы отредактировать и установить для всех элементов этого ввода одинаковое значение, нажмите или коснитесь здесь, в противном случае они сохранят свои индивидуальные значения."
+            },
+            "error": {
+                "system": "Возникла системная ошибка (<a target=\"\\\" rel=\"nofollow\" href=\"\\\">Подробнее<\/a>)."
+            }
+        },
+        "searchPlaceholder": "Что ищете?",
+        "stateRestore": {
+            "creationModal": {
+                "button": "Создать",
+                "search": "Поиск",
+                "columns": {
+                    "search": "Поиск по столбцам",
+                    "visible": "Видимость столбцов"
+                },
+                "name": "Имя:",
+                "order": "Сортировка",
+                "paging": "Страницы",
+                "scroller": "Позиция прокрутки",
+                "searchBuilder": "Редактор поиска",
+                "select": "Выделение",
+                "title": "Создать новое состояние",
+                "toggleLabel": "Включает:"
+            },
+            "removeJoiner": "и",
+            "removeSubmit": "Удалить",
+            "renameButton": "Переименовать",
+            "duplicateError": "Состояние с таким именем уже существует.",
+            "emptyError": "Имя не может быть пустым.",
+            "emptyStates": "Нет сохраненных состояний",
+            "removeConfirm": "Вы уверены, что хотите удалить %s?",
+            "removeError": "Не удалось удалить состояние.",
+            "removeTitle": "Удалить состояние",
+            "renameLabel": "Новое имя для %s:",
+            "renameTitle": "Переименовать состояние"
+        },
+        "thousands": " "
+    },
       columns: [
         { data: "id" },
         { data: "date" },
@@ -94,53 +314,7 @@ $(document).ready(function () {
         }
       },
       
-  
-      initComplete: function() {
-        var dataTable = this.api();
-  
-        // Add input elements to table headers
-        dataTable.columns().eq(0).each(function(colIndex) {
-          var columnHeader = $('.filters th').eq($(dataTable.column(colIndex).header()).index());
-          addInputToHeader(columnHeader);
-  
-          // Handle input change event
-          $('input', columnHeader).on('input', function(event) {
-            handleInputChange(dataTable, colIndex, this.value);
-          });
-        });
-  
-        // Add select elements to table footers
-        dataTable.columns().every(function(colIndex) {
-          var column = this;
-          var select = $('<select class="form-select"><option value=""></option></select>')
-            .appendTo($(column.footer()).empty())
-            .on('change', function() {
-              handleInputChange(dataTable, colIndex, '');
-            });
-          column.data().unique().sort().each(function(d, j) {
-            select.append('<option value="' + d + '">' + d + '</option>');
-          });
-        });
-      }
     });
-  
-    function addInputToHeader(header) {
-      var title = $(header).text();
-      $(header).html(`
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="${title}" />
-        </div>
-      `);
-    }
-  
-    function handleInputChange(dataTable, colIndex, value) {
-      var inputVal = value != '' ? '(((' + value + ')))' : '';
-      var selectVal = $(dataTable.column(colIndex).footer()).find('select').val();
-      var searchValue = inputVal + (selectVal ? '^' + selectVal + '$' : '');
-      dataTable.column(colIndex)
-        .search(searchValue, true, false)
-        .draw();
-    }
 
     // Форма добавления элемента
     $('#addForm').submit(function (event) {
