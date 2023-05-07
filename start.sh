8edd3386a1791976ccabc62ee8e6e293fc740ec0
ReDGamerR <super.koleda92@yandex.ru>
2023-05-08 00:46:25 +0300

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+echo "Запуск контейнера zeta-panel"
 if [ -f data/app.db ]; then
+    echo "Создание базы данных"
     flask db init
 fi
 
+echo "Обновление базы данных"
 flask db migrate
 flask db upgrade
+echo "Обновление базы данных завершено"
 
+echo "Запуск веб сервера"
 gunicorn wsgi:app \
     --bind 0.0.0.0:5000 \
     --workers $(nproc) \
