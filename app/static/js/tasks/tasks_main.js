2b68c0455262b2580353347e2936919d4d6f847b
root <root@HOME-PC>
2023-09-22 18:33:58 +0300

    fix data speed

--- app/static/js/tasks/tasks_main.js
+++ app/static/js/tasks/tasks_main.js
@@ -1,60 +1,70 @@
-$(document).ready(function () {
-  var apiEndpoint = '/api/tasks/get'; 
+$.ajax({
+  url: '/api/tasks/get', // Замените URL на адрес вашего сервера и ресурса
+  type: 'GET',
+  dataType: 'json', // Ожидаемый тип данных (может быть 'json', 'xml', 'html' и другие)
+  success: function(data) {
+    // Функция, которая выполняется при успешном выполнении запроса
+    console.log('Успешный запрос:');
+    table.rows.add(data);
+    table.draw();
+  },
+  error: function(xhr, status, error) {
+    // Функция, которая выполняется при ошибке запроса
+    console.error('Ошибка запроса:', status, error);
+  }
+});
 
-  // отправляем GET запрос на сервер и получаем данные
-  $.get(apiEndpoint, function (data) {
-    var table = $('#myTable').DataTable({
-      dom: '<"justify-content-between align-items-center"lfB><"table-responsive"rt><"justify-content-between align-items-center"ip>',
-      buttons: buttons,
-      stateSave: true,
-      responsive: true,
-      paging: true,
-      searching: true,
-      ordering: true,
-      data: data,
-      language: language,
-      columns: columns,
-      order: [[0, 'desc']],
 
-      createdRow: function(row, data, dataIndex) {
-        if (data.status === 'В Работе') {
-          $(row).addClass('table-primary');
-        }
-        if (data.status === 'Выполнено') {
-          $(row).addClass('table-success');
-        }
-        if (data.status === 'Отложено') {
-          $(row).addClass('table-danger');
-        }
-        if (data.status === 'Закрыто') {
-          $(row).addClass('table-info');
-        }
-      },
+// отправляем GET запрос на сервер и получаем данные
+var table = $('#myTable').DataTable({
+  dom: '<"justify-content-between align-items-center"lfB><"table-responsive"rt><"justify-content-between align-items-center"ip>',
+  buttons: buttons,
+  stateSave: true,
+  responsive: true,
+  paging: true,
+  searching: true,
+  ordering: true,
+  language: language,
+  columns: columns,
+  order: [[0, 'desc']],
 
-      rowCallback: function(row, data, index) {
-        $(row).on('dblclick', function(event) {
-          // Check if the clicked element is in the first column
-          if ($(event.target).closest('td').index() === 0) {
-            return; // Ignore clicks on the first column
-          }
-          // Get the ID of the clicked row
-          var id = $(row).find('td:eq(0)').text(); // Assumes the ID is in the first column
-          // Construct the URL and redirect to it
-          window.location.href = '/tasks/' + id;
-        })
-        // Double tap == Double click
-        $(row).doubletap(function(event) {
-          // Check if the clicked element is in the first column
-          if ($(event.target).closest('td').index() === 0) {
-            return; // Ignore clicks on the first column
-          }
-          // Get the ID of the clicked row
-          var id = $(row).find('td:eq(0)').text(); // Assumes the ID is in the first column
-          // Construct the URL and redirect to it
-          window.location.href = '/tasks/' + id;
-        })
+  createdRow: function(row, data, dataIndex) {
+    if (data.status === 'В Работе') {
+      $(row).addClass('table-primary');
+    }
+    if (data.status === 'Выполнено') {
+      $(row).addClass('table-success');
+    }
+    if (data.status === 'Отложено') {
+      $(row).addClass('table-danger');
+    }
+    if (data.status === 'Закрыто') {
+      $(row).addClass('table-info');
+    }
+  },
+
+  rowCallback: function(row, data, index) {
+    $(row).on('dblclick', function(event) {
+      // Check if the clicked element is in the first column
+      if ($(event.target).closest('td').index() === 0) {
+        return; // Ignore clicks on the first column
       }
-    });
-  });
+      // Get the ID of the clicked row
+      var id = $(row).find('td:eq(0)').text(); // Assumes the ID is in the first column
+      // Construct the URL and redirect to it
+      window.location.href = '/tasks/' + id;
+    })
+    // Double tap == Double click
+    $(row).doubletap(function(event) {
+      // Check if the clicked element is in the first column
+      if ($(event.target).closest('td').index() === 0) {
+        return; // Ignore clicks on the first column
+      }
+      // Get the ID of the clicked row
+      var id = $(row).find('td:eq(0)').text(); // Assumes the ID is in the first column
+      // Construct the URL and redirect to it
+      window.location.href = '/tasks/' + id;
+    })
+  }
 });
 
