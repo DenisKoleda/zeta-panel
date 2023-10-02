fedc48383e1af132e7d88ade8baf2aae011c1153
d.koleda <dkoleda@tp2-shift-work.02>
2023-10-02 20:58:01 +0300

    fix style for bootstrap

--- app/static/js/tasks/tasks_main.js
+++ app/static/js/tasks/tasks_main.js
@@ -33,7 +33,19 @@ $(document).ready(function() {
     dom: '<"justify-content-between align-items-center"lfB><"table-responsive"rt><"justify-content-between align-items-center"ip>',
     buttons: buttons,
     stateSave: true,
-    responsive: true,
+    responsive: {
+      details: {
+          display: DataTable.Responsive.display.modal({
+              header: function (row) {
+                  var data = row.data();
+                  return 'Details for ' + data[0] + ' ' + data[1];
+              }
+          }),
+          renderer: DataTable.Responsive.renderer.tableAll({
+              tableClass: 'table'
+          })
+      }
+  },
     pagingType: 'simple',
     searching: true,
     ordering: true,
